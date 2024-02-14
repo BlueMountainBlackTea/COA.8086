60cb3cf772e54e6fcbd64a4a34a1f035a971f523
BlueMountainBlackTea <141205429+BlueMountainBlackTea@users.noreply.github.com>
2024-02-14 22:30:30 +0530

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem Check if filename is provided
-if "%1"=="" echo Usage: %0 filename.asm
+if "%1"=="" goto illegal
 
 rem Set the filename from the command-line argument
 set filename=%1
@@ -18,6 +18,11 @@ if errorlevel 1 goto End
 echo executing %filename%.exe:
 
 %filename%.exe
+goto End
 
+:illegal
+echo Usage  : %0 <filename>
+echo Example: %0 helloWorld 
 :End
+
 
